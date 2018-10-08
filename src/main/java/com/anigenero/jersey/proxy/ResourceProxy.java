609d0253e8b5cf6a2faf993d2109bf5aa1bf2825
Robin Schultz <robin@anigenero.com>
2018-10-08 11:39:51 -0400

    fix imports

--- src/main/java/com/anigenero/jersey/proxy/ResourceProxy.java
+++ src/main/java/com/anigenero/jersey/proxy/ResourceProxy.java
@@ -3,12 +3,13 @@ package com.anigenero.jersey.proxy;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 
-import javax.decorator.Decorator;
-import javax.enterprise.inject.Stereotype;
 import javax.inject.Qualifier;
 import javax.ws.rs.client.ClientRequestFilter;
 import javax.ws.rs.client.ClientResponseFilter;
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Qualifier
 @Retention(RetentionPolicy.RUNTIME)
